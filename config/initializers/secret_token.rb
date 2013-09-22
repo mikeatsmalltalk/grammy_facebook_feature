a4f6146f9072ea444e50d7dd4cf1070691391811
mikeatsmalltalk <michael@smalltalkpublishing.com>
2013-09-21 22:31:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrammyFacebookFeature::Application.config.secret_token = 'e601b9b8fb1a908ddaac4ed2325b0faaf890d45af4f9b9b6d055f8d8bbd45987015b492e101d9f83ea8f3e0458ad64bdd47c98e1d02621eca2c06fad8a84e396'
